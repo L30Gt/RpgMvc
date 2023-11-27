c7ceb5a852ef1f30af2c1ff8f292fec6ae92cf05
L30Gt <leeo12322@hotmail.com>
2023-11-27 00:49:16 -0300

    feat: Added 'PersonagemHabilidade' Model, view and controller

---
+++ Controllers/PersonagemHabilidades.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using RpgMvc.Models;
+using Newtonsoft.Json;
+
+namespace RpgMvc.Controllers
+{
+    public class PersonagemHabilidades : Controller
+    {
+        public string uriBase = "http://leomiguel.somee.com/RpgApi/PersonagemHabilidades/";
+
+        [HttpGet("PersonagemHabilidades/{id}")]
+        public async Task<ActionResult> IndexAsync(int id)
+        {
+            try
+            {
+                HttpClient httpClient = new HttpClient();
+                string token = HttpContext.Session.GetString("SessionTokenUsuario");
+                httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+                HttpResponseMessage response = await httpClient.GetAsync(uriBase + id.ToString());
+                string serialized = await response.Content.ReadAsStringAsync();
+
+                if (response.StatusCode == System.Net.HttpStatusCode.OK)
+                {
+                    List<PersonagemHabilidadeViewModel> lista = await Task.Run(() =>
+                        JsonConvert.DeserializeObject<List<PersonagemHabilidadeViewModel>>(serialized));
+
+                    return View(lista);
+                }
+                else
+                    throw new System.Exception(serialized);
+            }
+            catch (System.Exception ex)
+            {
+                TempData["MensagemErro"] = ex.Message;
+                return RedirectToAction("Index");
+            }
+        }
+    }
+}

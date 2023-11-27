b2f071193a46bbc9e6153eab553e62398b0b5d94
L30Gt <leeo12322@hotmail.com>
2023-11-27 00:28:03 -0300

    feat: Added 'Disputas' index page

--- Models/DisputaViewModel.cs
+++ Models/DisputaViewModel.cs
@@ -9,10 +9,10 @@ namespace RpgMvc.Models
     {
         public int Id { get; set; }
         public DateTime? DataDisputa { get; set; }
-        public int AtacanteId { get; set; }
-        public int OponenteId { get; set; }
+        public int? AtacanteId { get; set; } = 0;
+        public int? OponenteId { get; set; } = 0;
         public string Narracao { get; set; } = string.Empty;
-        public int HabilidadeId { get; set; }
+        public int? HabilidadeId { get; set; } = 0;
         public List<int> ListaIdPersonagens { get; set; } = new List<int>();
         public List<string> Resultados { get; set; } = new List<string>();
     }
